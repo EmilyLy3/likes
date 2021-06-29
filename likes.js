a805f506c3903d3164297142a5f713bf750e3336
Emily Ly <em.a.ly63@gmail.com>
2021-06-29 19:00:29 -0400

    initial save

---
+++ likes.js
@@ -0,0 +1,26 @@
+// First user Neil's count
+var count1 = 9;
+var countElement1 = document.querySelector("#counter1");
+
+function increase1() {
+    count1++;
+    countElement1.innerHTML = count1 + " like(s)";
+};
+
+// Second user Nichole's count
+var count2 = 12;
+var countElement2 = document.querySelector("#counter2");
+
+function increase2() {
+    count2++;
+    countElement2.innerHTML = count2 + " like(s)";
+};
+
+// Third user Jim's count
+var count3 = 9;
+var countElement3 = document.querySelector("#counter3");
+
+function increase3() {
+    count3++;
+    countElement3.innerHTML = count3 + " like(s)";
+};
